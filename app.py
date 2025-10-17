751ff23d12a747ba03db760d3010e65c129d5ae1
Hayasshida <pedrohayashida9@gmail.com>
2025-10-17 02:44:52 -0300

    adicionando Cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 import uuid
 from services.parking_service import cadastrarCarro, consultarCarros, removerCarro, update_car
+from flask_cors import CORS
 
 app = Flask(__name__) 
+
+CORS(app)
 
 @app.route("/carros", methods=["POST"])
 def cadastrar():
